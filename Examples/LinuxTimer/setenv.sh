0d54fa6d556c65289e8f3ac6b38bb94de6af2f64
Adam V. Stephen (WSL U2204 L1254) <adam.stephen@ukaea.uk>
2024-10-05 12:32:03 +0100

    Added further examples relating to Inverted Pendulum and demonstrating LinuxTimer

---
+++ Examples/LinuxTimer/setenv.sh
@@ -0,0 +1,6 @@
+export MARTe2_ACTIVE_PROJECT=$(pwd)/
+#export MARTe2_Components_DIR=$(pwd)/MARTe2-components
+#export MARTe2_DIR=$(pwd)/MARTe2
+export MARTe2_UTILS=/home/astephen/git-wd/InvertedPendulumTestA/MARTe2-utils
+export PATH=$PATH:$(pwd)/MARTe2-utils/bin
+source "$MARTe2_UTILS/bin/m2-completion.bash" 
